0fd16a4901827d3465c3a62c06e43713e6d05731
root <root@localhost.localdomain>
2015-12-23 10:06:05 +0800

    vms

--- study/xmldom_learn.py
+++ study/xmldom_learn.py
@@ -3,12 +3,39 @@
 
 import xml.dom.minidom
 
-dom = xml.dom.minidom.parse("catlog.xml")
+#dom = xml.dom.minidom.parse("catlog.xml")
 
-print dom
+#print dom
 
-root = dom.documentElement
+#root = dom.documentElement
 
-print root
+#print root
 
-print root.nodeName
+#print root.nodeName
+
+def _get_Vm():
+    dom = xml.dom.minidom.parse("vm.xml")
+    root = dom.documentElement
+    return root
+
+def getVm_Name():
+    root = _get_Vm()
+    bb = root.getElementsByTagName("name")
+    b = bb[0]
+    print b.firstChild.data
+
+def getVm_ID():
+    root = _get_Vm()
+    vm_ids = root.getElementsByTagName("vm")
+    vm_id = vm_ids[0]
+    print vm_id.getAttribute("id")
+
+
+
+if __name__ == "__main__":
+    
+    getVm_Name()    
+    getVm_ID()
+
+
+
